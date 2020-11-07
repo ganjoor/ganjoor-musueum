a1c4cc2dee3ecd4a35cd79ba7cbbca0c37d960f6
Iman Abidi <imanabidi@gmail.com>
2020-11-07 12:44:30 +0100

    better client side validation in signup forms

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,10 +7,11 @@ import fa from 'vuetify/es5/locale/fa';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    lang: {
-      locales: { fa },
-      current: 'fa',
-    },
+  rtl: true,
+  lang: {
+    locales: { fa },
+    current: 'fa',
+  },
   icons: {
     iconfont: 'mdi',
   },
