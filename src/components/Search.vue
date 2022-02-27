15cc818288d426bda92000f966a818753d32938b
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-02-27 19:25:16 +0330

    docs: #4 searching through items

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -25,7 +25,7 @@
             <v-layout row wrap>
               <v-flex v-for="item in artifacts" :key="item.id" d-flex>
                 <v-card flat raised dark class="d-flex" style="width: 200px">
-                  <router-link :to="`/artifacts/${item.friendlyUrl}`">
+                  <router-link :to="`/items/${item.friendlyUrl}`">
                     <v-img
                       :src="`${appConfig.$api_url}/api/images/thumb/${item.coverImage.id}.jpg`"
                       width="200px"
@@ -50,6 +50,53 @@
           </v-container>
         </v-card>
       </v-flex>
+
+      <v-flex xs4>
+        <v-card>
+          <v-flex v-if="errorMsgItems != ''" xs12>
+            <v-card dark color="secondary">
+              <v-card-text>{{ errorMsgItems }}</v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex v-if="loadingItems" xs12>
+            <v-card dark color="secondary">
+              <v-progress-circular indeterminate></v-progress-circular>
+            </v-card>
+          </v-flex>
+          <v-card-text v-if="!loadingItems && items != null">
+            {{ items.length }} تصویر
+          </v-card-text>
+          <v-container v-if="items != null" grid-list-sm fluid>
+            <v-layout row wrap>
+              <v-flex v-for="item in items" :key="item.id" d-flex>
+                <v-card flat raised dark class="d-flex" style="width: 200px">
+                  <router-link
+                    :to="`/item/${item.parentFriendlyUrl}/${item.item.friendlyUrl}`"
+                  >
+                    <v-img
+                      :src="`${appConfig.$api_url}/api/images/thumb/${item.item.images[0].id}.jpg`"
+                      width="200px"
+                      class="grey lighten-2 clickable"
+                    >
+                      <template v-slot:placeholder>
+                        <v-layout fill-height align-center justify-center ma-0>
+                          <v-progress-circular
+                            indeterminate
+                            color="grey lighten-5"
+                          ></v-progress-circular>
+                        </v-layout>
+                      </template>
+                    </v-img>
+                    <v-card-subtitle style="color: lightgray">
+                      {{ item.item.name }}
+                    </v-card-subtitle>
+                  </router-link>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -63,14 +110,17 @@ export default {
     return {
       term: null,
       artifacts: [],
-      artifactsLength: 0,
       loadingArtifacts: true,
       errorMsgArtifacts: "",
+      items: [],
+      loadingItems: true,
+      errorMsgItems: "",
     };
   },
   mounted() {
-    this.loadingArtifacts = true;
     this.term = this.$route.query.q;
+
+    this.loadingArtifacts = true;
     axios({
       method: "GET",
       url:
@@ -82,13 +132,34 @@ export default {
     }).then(
       (result) => {
         this.artifacts = result.data;
-        this.artifactsLength = this.artifacts.length;
 
         this.loadingArtifacts = false;
       },
       (error) => {
         this.errorMsgArtifacts = error;
         this.loadingArtifacts = false;
+      }
+    );
+
+    this.loadingItems = true;
+
+    axios({
+      method: "GET",
+      url:
+        this.appConfig.$api_url +
+        "/api/artifacts/search/items?term=" +
+        encodeURIComponent(this.term),
+      data: {},
+      headers: { "content-type": "application/json" },
+    }).then(
+      (result) => {
+        this.items = result.data;
+
+        this.loadingItems = false;
+      },
+      (error) => {
+        this.errorMsgItems = error;
+        this.loadingItems = false;
       }
     );
   },
